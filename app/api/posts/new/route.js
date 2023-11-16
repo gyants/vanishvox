dc5ab620526373807583ecd535e9e1832c2ed45c
GIANT <isiri.sriuthai@gmail.com>
2023-11-17 03:28:34 +0700

    dynamic loading

--- app/api/posts/new/route.js
+++ app/api/posts/new/route.js
@@ -1,6 +1,10 @@
 import { connectToDB } from "@/lib/mongoose";
 import Post from "@/lib/models/post";
 
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export const POST = async(req) => {
     const { message } = await req.json();
 
--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,6 +1,10 @@
 import { connectToDB } from "@/lib/mongoose";
 import Post from "@/lib/models/post";
 
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export const GET = async (request) => {
     try {
         await connectToDB();
