c2a05425c3e8b41ec963cb22d7fe4e5f975685d2
GIANT <isiri.sriuthai@gmail.com>
2023-11-17 01:55:55 +0700

    posting and fetching done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        
+        "purple-500": "#7D38B3",
+        "purple-700": "#4B0082",
+        "gold-500": "#FFD700",
+        "gold-800": "#917800",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    fontFamily: {
+      playfairDisplay: ['Playfair Display', 'serif'],
+      raleway: ['Raleway', 'sans-serif'],
     },
   },
   plugins: [],
